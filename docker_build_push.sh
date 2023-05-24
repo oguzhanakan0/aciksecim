50ab71b6278c817baf85cf9485b3ec6bf0f54dc3
Oguzhan Akan <37854131+oguzhanakan0@users.noreply.github.com>
2023-05-24 01:22:32 -0400

    Update docker_build_push.sh
    masking purposes

--- docker_build_push.sh
+++ docker_build_push.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 timestamp=$(date +%s)
-image="europe-west1-docker.pkg.dev/data-rookery-387723/aciksecim-gar/aciksecim:${timestamp}"
+image="${timestamp}"
 docker build -t ${image} .
 docker push ${image}
